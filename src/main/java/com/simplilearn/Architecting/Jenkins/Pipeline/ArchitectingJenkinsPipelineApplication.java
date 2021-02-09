d759a5c5a7635993be27378d2546099a14a81948
student <alidia87@gmail.com>
2021-02-09 22:47:51 +0000

    Dummy Maven Project

---
+++ src/main/java/com/simplilearn/Architecting/Jenkins/Pipeline/ArchitectingJenkinsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Architecting.Jenkins.Pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectingJenkinsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectingJenkinsPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/Architecting/Jenkins/Pipeline/ArchitectingJenkinsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Architecting.Jenkins.Pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectingJenkinsPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
